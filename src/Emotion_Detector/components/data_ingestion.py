81a74d70f2ca8f8548f7f7ff59365dca5961510a
Suraj Verma <stdmail2552@gmail.com>
2023-07-14 00:09:19 +0530

    Stage-01 Data Ingestion - Logging Added

--- src/Emotion_Detector/components/data_ingestion.py
+++ src/Emotion_Detector/components/data_ingestion.py
@@ -11,20 +11,20 @@ from tqdm import tqdm
 
 class DataIngestion:
     def __init__(self, config: DataIngestionConfig):
-        logger.info(f"{'>>>>'*5} Getting Configurations {'<<<<'*5}.")
+        logger.info(f"Getting Configurations...")
         self.config = config
 
     def download_file(self):
-        logger.info(f"{'>>>>'*5} Downloading the zip file from the url - {self.config.source_url} \
-                    to file - {self.config.local_data_file} {'<<<<'*5}.")
+        logger.info(f"Downloading the zip file from the url - {self.config.source_url} \
+                    to file - {self.config.local_data_file} ...")
         if not os.path.exists(self.config.local_data_file):
             filename, headers= request.urlretrieve(
                 url = self.config.source_url,
                 filename = self.config.local_data_file
             )
-            logger.info(f"{'>>>>'*5} Zip File Downloaded and saved at {self.config.local_data_file} {'<<<<'*5}.")
+            logger.info(f"Zip File Downloaded and saved at {self.config.local_data_file}.")
         else:
-            logger.info(f"{'>>>>'*5} File already present in {self.config.local_data_file} {'<<<<'*5} of size\
+            logger.info(f" File already present in {self.config.local_data_file} of size\
                          {get_size(Path(self.config.local_data_file))}.")
 
     def _preprocess(self, zf : ZipFile, f : str, working_dir : str):
@@ -36,10 +36,10 @@ class DataIngestion:
             os.remove(target_filepath)
 
     def unzip_and_clean(self):
-        logger.info(f"{'>>>>'*5} Extracting the zip file.{'<<<<'*5}.")
+        logger.info(f"Extracting the zip file...")
         with ZipFile(file = self.config.local_data_file, mode="r") as zf:
             list_of_files = zf.namelist()
             for f in tqdm(list_of_files):
                 self._preprocess(zf, f, self.config.unzip_dir)
-        logger.info(f"{'>>>>'*5} Successfully extracted data from the zip file and saved a location - \
-                    {self.config.unzip_dir} {'<<<<'*5}.")
+        logger.info(f"Successfully extracted data from the zip file and saved a location - \
+                    {self.config.unzip_dir}")
