908b306a416bb5d021d938d5641e8ef7b24644ef
Suraj Verma <stdmail2552@gmail.com>
2023-07-14 00:12:29 +0530

    Configurations Updated For Stage-02

--- src/Emotion_Detector/config/configuration.py
+++ src/Emotion_Detector/config/configuration.py
@@ -1,6 +1,6 @@
 from Emotion_Detector.utils import *
 from Emotion_Detector.constants import *
-from Emotion_Detector.entity.config_entity import (DataIngestionConfig)
+from Emotion_Detector.entity.config_entity import (DataIngestionConfig, DataPreprocessingConfig)
 
 
 class ConfigurationManager:
@@ -27,3 +27,33 @@ class ConfigurationManager:
             unzip_dir = config.unzip_dir
         )
         return data_ingestion_config
+    
+    def get_data_preprocessing_config(self) -> DataPreprocessingConfig:
+        config = self.config.data_preprocessing
+
+
+        create_directories([config.root_dir])
+        create_directories([config.train_tf_records_dir])
+        create_directories([config.test_tf_records_dir])
+
+        data_preprocessing_config = DataPreprocessingConfig(
+            root_dir = Path(config.root_dir),
+            train_dir = Path(config.train_dir),
+            val_dir = Path(config.val_dir),
+            train_tf_records_dir = Path(config.train_tf_records_dir),
+            test_tf_records_dir = Path(config.test_tf_records_dir),
+            train_tf_records_file_path = config.train_tf_records_file_path,
+            test_tf_records_file_path = config.test_tf_records_file_path,
+            params_batch_size = self.params.BATCH_SIZE,
+            params_class_names = self.params.CLASS_NAMES,
+            params_image_size = self.params.IMAGE_SIZE,
+            params_shuffle = self.params.SHUFFLE,
+            params_random_seed = self.params.RANDOM_SEED,
+            params_train_num_shards = self.params.TRAIN_NUM_SHARDS,
+            params_test_num_shards = self.params.TEST_NUM_SHARDS,
+            param_random_rotation_left_factor = self.params.RANDOM_ROTATION_LEFT_FACTOR,
+            param_random_rotation_right_factor = self.params.RANDOM_ROTATION_RIGHT_FACTOR,
+            param_random_flip_mode = self.params.RANDOM_FLIP_MODE,
+            param_random_contrast_factor = self.params.RANDOM_CONTRAST_FACTOR
+        )
+        return data_preprocessing_config
