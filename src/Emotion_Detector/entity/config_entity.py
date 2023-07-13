f60dcb973d4c5c506f4b63903b0ba645ca6be457
Suraj Verma <stdmail2552@gmail.com>
2023-07-14 00:12:05 +0530

    Config Entity Updated For Stage-02

--- src/Emotion_Detector/entity/config_entity.py
+++ src/Emotion_Detector/entity/config_entity.py
@@ -9,3 +9,25 @@ class DataIngestionConfig:
     local_data_dir: Path
     local_data_file: Path
     unzip_dir: Path
+
+
+@dataclass(frozen=True)
+class DataPreprocessingConfig:
+    root_dir: Path
+    train_dir: Path
+    val_dir: Path
+    train_tf_records_dir : Path
+    test_tf_records_dir : Path
+    train_tf_records_file_path : str
+    test_tf_records_file_path : str
+    params_batch_size : int
+    params_class_names : list
+    params_image_size : int
+    params_shuffle : bool
+    params_random_seed : int
+    params_train_num_shards : int
+    params_test_num_shards : int
+    param_random_rotation_left_factor: float
+    param_random_rotation_right_factor: float
+    param_random_flip_mode: str
+    param_random_contrast_factor: float
