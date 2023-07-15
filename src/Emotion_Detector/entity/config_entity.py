c53976f38651d7cfdc5411c4d7a9fb12ca825d33
Suraj Verma <stdmail2552@gmail.com>
2023-07-15 13:31:18 +0530

    Config Entity Updated For Stage-03

--- src/Emotion_Detector/entity/config_entity.py
+++ src/Emotion_Detector/entity/config_entity.py
@@ -31,3 +31,23 @@ class DataPreprocessingConfig:
     param_random_rotation_right_factor: float
     param_random_flip_mode: str
     param_random_contrast_factor: float
+
+@dataclass(frozen=True)
+class ModelTrainingConfig:
+    root_dir: Path
+    checkpoints_callback_dir : Path
+    tensorboard_log_dir : Path
+    model_history_dir : Path
+    model_dir : Path
+    model_file_path : str
+    checkpoints_file_path : str
+    model_history_file_path : Path
+    train_tf_records_file_path : str
+    test_tf_records_file_path : str
+    param_image_size : int
+    param_learning_rate : float
+    param_epochs : int
+    param_train_num_shards : int
+    param_test_num_shards : int
+    param_batch_size : int
+    all_params : dict
